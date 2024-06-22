7bd7610a9c1d95008758808c67f4d732cd711140
Ritik <rajritik90@gmail.com>
2024-06-22 09:08:58 +0530

    fist commit

--- project.js
+++ project.js
@@ -1,1 +1,7 @@
 console.log("My first project")
+const accountId = 90230
+let account_Password = "12345"
+accoundCity = "jaipur"
+console.log(accountId);
+console.log(accoundCity);
+console.log(account_Password);
